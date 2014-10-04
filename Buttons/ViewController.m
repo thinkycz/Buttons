a78c0828647639af4e23e300efad3286cf9a4fe4
thinkycz <thinky@Thinky-iMac.local>
2014-10-04 11:28:03 +0200

    First commit

--- Buttons/ViewController.m
+++ Buttons/ViewController.m
@@ -9,10 +9,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *textMessage;
 @end
 
 @implementation ViewController
+
+- (IBAction)buttonClick:(UIButton *)sender
+{
+    if ([[[sender titleLabel] text] isEqual:@"One"])
+    {
+        [self.textMessage setText:[[NSString stringWithFormat:@"You touched the blue button."] capitalizedString]];
+    }
+    else if ([[[sender titleLabel] text] isEqual:@"Two"])
+    {
+        [self.textMessage setText:[[NSString stringWithFormat:@"You touched the red button."] uppercaseString]];
+    }
+    else if ([[[sender titleLabel] text] isEqual:@"Three"])
+    {
+        [self.textMessage setText:[[NSString stringWithFormat:@"You touched the green button."] lowercaseString]];
+    }
+    else if ([[[sender titleLabel] text] isEqual:@"Four"])
+    {
+        [self.textMessage setText:[[NSString stringWithFormat:@"You touched the orange button."] stringByAppendingString:@" Append"]];
+    }
+}
+
+- (IBAction)sliderChange:(UISlider *)sender
+{
+    [self.textMessage setText:[NSString stringWithFormat:@"The slider value is: %d", (int)roundf([sender value])]];
+}
 
 - (void)viewDidLoad
 {
